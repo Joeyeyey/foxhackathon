606f3f352231b073efad8307e847bdd8428ac97f
JaLeeZeeY <JaLeeZeeY@JaLeeZeeYs-MacBook-Air.local>
2018-11-11 17:31:46 -0800

    I dont remember what ive done anymore

--- foxhackathon2/server/static/foxhackathon2/javascript/base.js
+++ foxhackathon2/server/static/foxhackathon2/javascript/base.js
@@ -17,23 +17,27 @@ function addItem() {
      //Target Tag: <input type="text" id="output" value=user_input readonly>
      text_box1.setAttribute("id", "out_name");
      text_box2.setAttribute("id", "out_revenue");
+     text_box1.setAttribute("class", "textfield");
+     text_box2.setAttribute("class", "textfield");
 
      text_box1.setAttribute("type", "text");
      text_box2.setAttribute("type", "text");
      text_box1.setAttribute("value", name.value);
-     text_box1.setAttribute("value", revenue.value);
+     text_box2.setAttribute("value", revenue.value);
 
      document.body.appendChild(text_box1);
      document.body.appendChild(text_box2);
 
      document.getElementById("out_name").readOnly = true;
      document.getElementById("out_revenue").readOnly = true;
-     document.createElement("br");
      //name.setAttribute("readonly");
-
 }
 
 function removeItem() {
      //This function will remvove 2 rows of user_input from the page
+     var name = document.getElementById("name");
+     var revenue = document.getElementById("revenue");
 
+     name.parentNode.removeChild(name);
+     revenue.parentNode.removeChild(revenue);
 }
--- foxhackathon2/server/views.py
+++ foxhackathon2/server/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import Employee
+import csv
 
 def index(request):
     #check for existing employees
