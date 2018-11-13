0dd3e63cdfeb7ea6167274070403cd643cb3f255
JaLeeZeeY <JaLeeZeeY@JaLeeZeeYs-MacBook-Air.local>
2018-11-12 17:48:43 -0800

    fixed employee manager to be used before submission to database

--- foxhackathon2/server/static/foxhackathon2/javascript/base.js
+++ foxhackathon2/server/static/foxhackathon2/javascript/base.js
@@ -6,38 +6,29 @@ function closeNav() {
     document.getElementById("sidebar").style.width = "0px";
 }
 
-function addItem() {
-     //This function will add 2 rows of user_input into the page
-     var text_box1 = document.createElement("INPUT");
-     var text_box2 = document.createElement("INPUT");
+//This function will add items when the addition button is clicked
+$(document).ready(function() {
+     var counter = 0
 
-     var name = document.getElementById("in_name");
-     var revenue = document.getElementById("in_revenue");
+     //Add item
+     $("#add").click(function() {
+          var name = $("#in_name").val();
+          var revenue = $("#in_revenue").val();
 
-     //Target Tag: <input type="text" id="output" value=user_input readonly>
-     text_box1.setAttribute("id", "out_name");
-     text_box2.setAttribute("id", "out_revenue");
-     text_box1.setAttribute("class", "textfield");
-     text_box2.setAttribute("class", "textfield");
+          $("#added_employees").append(
+               "<div id=employee" + counter + ">" +
+               "<input type='text' class='added_employee' id='employee" + counter + '\' value="' + name + '" readonly>' +
+               "<input type='text' class='added_employee' id='employee" + counter + '\' value="' + revenue + '" readonly>' +
+               "<input type='button' class='added_employee' value='X' id='employee" + counter + "\'></div>"
+          );
+          name = 0;
+          revenue = 0;
+          counter++;
+     });
 
-     text_box1.setAttribute("type", "text");
-     text_box2.setAttribute("type", "text");
-     text_box1.setAttribute("value", name.value);
-     text_box2.setAttribute("value", revenue.value);
+     $("#added_employees").on('click', ".added_employee", function() {
+          var employee_id = $(this).attr('id');
 
-     document.body.appendChild(text_box1);
-     document.body.appendChild(text_box2);
-
-     document.getElementById("out_name").readOnly = true;
-     document.getElementById("out_revenue").readOnly = true;
-     //name.setAttribute("readonly");
-}
-
-function removeItem() {
-     //This function will remvove 2 rows of user_input from the page
-     var name = document.getElementById("name");
-     var revenue = document.getElementById("revenue");
-
-     name.parentNode.removeChild(name);
-     revenue.parentNode.removeChild(revenue);
-}
+          $("#" + employee_id).remove();
+     });
+});
--- foxhackathon2/server/views.py
+++ foxhackathon2/server/views.py
@@ -8,9 +8,11 @@ def index(request):
     #check for existing employees
     #display the employess in the database
     num_existing_employees = Employee.objects.order_by('id')
+    list_employees = Employee.objects.all()
 
     template = loader.get_template('server/index.html')
     context = {
         'num_existing_employees': num_existing_employees,
+        'list_employees': list_employees,
     }
     return HttpResponse(template.render(context, request))
